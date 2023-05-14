288ae902645e10add06311745d4c403ab981ae5f
nugoori <tig01610@naver.com>
2023-05-14 20:01:17 +0900

    apis auth 파일옴기기

--- front/board-react/src/apis/request/auth/index.ts
+++ front/board-react/src/apis/request/auth/index.ts
@@ -1,3 +1,6 @@
 import SignUpDto from "./Sign-up.request.dto";
+import SignInDto from './Sign-in.request.dto';
+import ValidateEmailDto from "./Validate-email.request.dto";
+import ValidateNicknameDto from "./Validate-nickname.request.dto";
 
-export type { SignUpDto };
+export type { SignUpDto, SignInDto, ValidateEmailDto, ValidateNicknameDto };
